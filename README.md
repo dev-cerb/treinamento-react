a74f190a864578640f31460b08c1b65b0ba6e822
Carlos Barreto <contato.cerb@outlook.com>
2024-04-22 20:47:00 -0300

    subindo para o github

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sobre o Projeto 
+
+Projeto feito durante a aprendizagem da framework React. Autoria do Projeto pertence ao canal do youtube Matheus Battisti - Hora de Codar. 
+
+Projeto ainda em construção.
+
---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,22 @@
+import Input from '../form/Input';
+import Select from '../form/Select';
+import SubmitButton from '../form/SubmitButton';
+import styles from './ProjectForm.module.css';
+
+export default function ProjectForm({btnText}){
+
+    return(
+        <form className={styles.form}>
+            <Input type="text" text="Nome do projeto" 
+            name="name" placeholder="Insira nome do projeto"/>
+            
+            
+            <Input type="number" text="Orçamento do projeto" 
+            name="budget" placeholder="Insira orçamento do projeto"/>
+
+            <Select name="category_id" text="Selecione a categoria"/>
+
+            <SubmitButton text={btnText} />
+        </form>
+    )
+}
