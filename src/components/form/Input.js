e4b5ba05360edd7d4fe98be8881ab76f95db952f
Carlos Barreto <contato.cerb@outlook.com>
2024-05-01 22:49:05 -0300

    feat::adicionando jsonserver e criacao de projeto

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,6 +1,7 @@
 import styles from './Input.module.css';
 
 export default function Input({type, text, name, placeholder, handleOnChange, value}){
+
     return(
         <div className={styles.form_control}>
             <label htmlFor={name}> {text}: </label>
